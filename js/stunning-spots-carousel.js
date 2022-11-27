0fb23c9e860d2f122e11c2286a9a34531bb21f3b
Konstantin-Mazykin <konstantin.mazykin@gmail.com>
2022-11-27 19:09:19 +0200

    New functions in JS

--- js/stunning-spots-carousel.js
+++ js/stunning-spots-carousel.js
@@ -1,4 +1,5 @@
 (() => {
+
     const slides = [
         '<div><img src="img/port-in-norway-sm.jpg" alt="Port in Norway"></div>',
         '<div><img src="img/iceberg-in-the-sea-sm.jpg" alt="Iceberg in the sea"></div>',
@@ -6,7 +7,8 @@
         '<div><img src="img/observation-deck-on-the-fjords-sm.jpg" alt="Observation deck on the fjords"></div>',
         '<div><img src="img/village-by-the-lake-sm.jpg" alt="Village by the lake"></div>',
         '<div><img src="img/red-houses-on-the-beach-sm.jpg" alt="Red houses on the beach"></div>',
-    ]
+        '<div><img src="img/man-with-backpack-in-the-mountains.jpg" alt="Man with a backpack in the mountains"></div>',
+    ];
 
     let currentSlideIdx = 0;
 
@@ -21,7 +23,20 @@
                 slideContainer.innerHTML += slides[thirdSlideIdx];
             }
         }
+        renderButtons()
     }
+
+    function renderButtons() {
+        const buttonsContainer = document.querySelector('.carousel-indicators');
+        buttonsContainer.innerHTML = "";
+        let divContent;
+        for (let i = 0; i < slides.length; i++) {
+            if (i === currentSlideIdx) { divContent = '<div><button id="push_button" type="button" value="' + i + '"></button></div>'; }
+            else { divContent = '<div><button type="button" value="' + i + '"></button></div>'; }
+            buttonsContainer.innerHTML += divContent;
+        }
+    }
+
     function nextSlide() {
         currentSlideIdx++;
         if (currentSlideIdx >= slides.length) currentSlideIdx = 0;
@@ -35,36 +50,20 @@
     }
 
     function getValue(btn) {
-        currentSlideIdx = parseInt(btn.target.value) - 1;
+        const currentValue = btn.target.value;
+        if (currentValue !== undefined) currentSlideIdx = parseInt(currentValue);
         renderSlide();
     }
 
     renderSlide();
-
+    
     const nextButton = document.querySelector('.stunning-spots__carousel .stunning-spots__carousel_btn-next');
     nextButton.addEventListener('click', nextSlide);
 
     const prevButton = document.querySelector('.stunning-spots__carousel .stunning-spots__carousel_btn-prev');
     prevButton.addEventListener('click', prevSlide);
 
-    const selectButton01 = document.querySelector('.carousel-indicators #carousel-button-01');
-    selectButton01.addEventListener('click', getValue);
-
-    const selectButton02 = document.querySelector('.carousel-indicators #carousel-button-02');
-    selectButton02.addEventListener('click', getValue);
-
-    const selectButton03 = document.querySelector('.carousel-indicators #carousel-button-03');
-    selectButton03.addEventListener('click', getValue);
-
-    const selectButton04 = document.querySelector('.carousel-indicators #carousel-button-04');
-    selectButton04.addEventListener('click', getValue);
-
-    const selectButton05 = document.querySelector('.carousel-indicators #carousel-button-05');
-    selectButton05.addEventListener('click', getValue);
-
-    const selectButton06 = document.querySelector('.carousel-indicators #carousel-button-06');
-    selectButton06.addEventListener('click', getValue);
-
-    window.addEventListener('resize', renderSlide);
+    const selectButton = document.querySelector('.carousel-indicators');
+    selectButton.addEventListener('click', getValue);
 
 })();
